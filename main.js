2be99d253f7dd5aba18a356a2b5110ebfc5d9fe7
enzo <enzoperce35@gmail.com>
2020-05-06 07:22:30 +0800

    Improved the codes for lag prevention

--- main.js
+++ main.js
@@ -1,58 +1,57 @@
 var button = document.createElement('button');
-    button.textContent = "Reset the Sketchpad";
-     button.setAttribute('id', 'button')
+  button.textContent = "Reset the Sketchpad";
+    button.setAttribute('id', 'button')
       document.body.append(button);
 
 function createMain(number) {
-  if(number < 101)
-    mainContainer = document.createElement('div');
-     mainContainer.setAttribute('id', 'mainContainer');
-      document.body.append(mainContainer);     
+       if(number < 101)
+         mainContainer = document.createElement('div');
+          mainContainer.setAttribute('id', 'mainContainer');
+           document.body.append(mainContainer);     
 }
 createMain(1);
 
+let varArray = [];
+
 function createDiv(number) {
-  if (number < 101) {
-        width = 500/number-2;
-        height = 500/number-2;
-        number = number*number;
-const varArray = [];
-  for (i=0; i<number; i++) {
-    var singleDiv = document.createElement('div');
-     varArray.push(singleDiv); 
-    varArray.forEach((i) => {
-        i.setAttribute('class', 'squares');
-        i.setAttribute('style', `width: ${width}px; height: ${height}px`);
-        document.getElementById("mainContainer").appendChild(i);  
-});
-} 
-}else {
+  number = number*number;
+       for (i=0; i<number; i++) {
+        var singleDiv = document.createElement('div');
+          varArray.push(singleDiv); 
 } 
 }
-createDiv(16);
+createDiv(100);
 
-  var firstSquares = document.querySelectorAll(".squares");
-firstSquares.forEach((i) => {
-      i.setAttribute('class', 'firstSquares');
-           i.addEventListener('mouseover', (e) =>{
-            i.style.backgroundColor = "green";
+function sliceVarArray(number) {
+  if (number < 101) {
+    width = 500/number-2;
+    height = 500/number-2;
+    number = number*number;
+         firstSquares = varArray.slice(0, number);
+           firstSquares.forEach( function(i) {
+            document.getElementById("mainContainer").appendChild(i);
+             i.setAttribute('class', 'squares');
+             i.setAttribute('style', `width: ${width}px; height: ${height}px`);
+             i.addEventListener('mouseover', (e) =>{
+             i.style.backgroundColor = "green";
 })
 });
+}else {
+}
+}
+sliceVarArray(16);
 
-  var resetButton = document.getElementById('button');
+var resetButton = document.getElementById('button');
 resetButton.addEventListener('click', function()  {
-        document.getElementById("mainContainer").remove();
-           answer =  prompt('Please enter grid size', '1-100'); {
-                 createMain(answer);
-                  createDiv(answer);
+         document.getElementById("mainContainer").remove();
+          answer = prompt('Please enter grid size', '1-100'); {
+          createMain(answer);
+          sliceVarArray(answer);
 };
-    
-    let blocks = document.querySelectorAll('.squares');
-blocks.forEach((i) => {
-       i.addEventListener('mouseover', (e) =>{
-       i.style.backgroundColor = "green";
+  let blocks = document.querySelectorAll('.squares');
+         blocks.forEach((i) => {
+          i.addEventListener('mouseover', (e) =>{
+          i.style.backgroundColor = "green";
 })
 })
 });
-  
-     
